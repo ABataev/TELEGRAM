c37104fe576e79c22da48a48db2147935208fc7b
ABataev <152414440+ABataev@users.noreply.github.com>
2024-04-27 15:11:36 +0300

    Add files via upload

---
+++ data/score.py
@@ -0,0 +1,22 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Score(SqlAlchemyBase):
+    __tablename__ = 'score'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                )
+
+    chat_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                )
+
+    film_id = sqlalchemy.Column(sqlalchemy.Integer)
+
+    score = sqlalchemy.Column(sqlalchemy.Integer)
+
+    genre = sqlalchemy.Column(sqlalchemy.String)
